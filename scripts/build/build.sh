6ec53c119f2f45c14bdaa2c82d6a8d0e71d30711
bergquist <carl.bergquist@gmail.com>
2017-01-30 17:37:38 +0100

    disable unit tests until we solved packaging

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -10,18 +10,27 @@ REPO_PATH=$GOPATH/src/github.com/grafana/grafana
 mkdir -p /go/src/github.com/grafana
 cd /go/src/github.com/grafana
 
-if [ -z "$CIRCLE_TAG" ]; then
-  echo "Building from branch $CIRCLE_BRANCH"
+if [ "$CIRCLE_TAG" != "" ]; then
+  echo "Builing from tag $CIRCLE_TAG"
   git clone https://github.com/grafana/grafana.git
   cd $REPO_PATH
   git checkout $CIRCLE_TAG
 else
-  echo "Builing from tag $CIRCLE_TAG"
+  echo "Building from branch $CIRCLE_BRANCH"
   git clone --depth 1 https://github.com/grafana/grafana.git -b $CIRCLE_BRANCH
   cd $REPO_PATH
 fi
 
-go run build.go build
+echo "current dir: $(pwd)"
+
+if [ "$CIRCLE_TAG" != "" ]; then
+  echo "Building incremental build for master"
+  go run build.go build
+else
+  echo "Building a release"
+  go run build.go -includeBuildNumber=false build
+fi
+
 yarn install --pure-lockfile
 
 source /etc/profile.d/rvm.sh
@@ -29,10 +38,14 @@ rvm use 2.1.9 --default
 
 gem install fpm -v 1.4
 
-if [ -z "$CIRCLE_TAG" ]; then
-  go run build.go -includeBuildNumber=false package latest
-else
+echo "current dir: $(pwd)"
+
+if [ "$CIRCLE_TAG" != "" ]; then
+  echo "Building incremental build for master"
   go run build.go package latest
+else
+  echo "Building a release"
+  go run build.go -includeBuildNumber=false package latest
 fi
 
 cp dist/* /tmp/dist/
